f11aeda82d70fe47958935c8c9bd94f2f5abe5a4
dkobran <dkobran@users.noreply.github.com>
2021-09-16 18:12:53 -0400

    Update api_test.py

--- api_test.py
+++ api_test.py
@@ -1,22 +1,31 @@
 import requests
 import json
 
-url = "http://127.0.0.1:4000/prediction"
+# Endpoint URL
+url = "https://clzut41m4.paperspacegradient.com/model-serving/des4z1prl34tt5o:predict"
 
+# Request
 payload = json.dumps({
-  "acousticness": 0.344719513,
-  "danceability": 0.758067547,
-  "energy": 0.323318405,
-  "instrumentalness": 0.0166768347,
-  "liveness": 0.0856723112,
-  "speechiness": 0.0306624283,
-  "tempo": 101.993,
-  "valence": 0.443876228
+  "customer": "u6746rt67h78",
+  "transaction": {
+    "transaction_amt": "410.0",
+    "ip_add": "98.116.195.133",
+    "billing_city": "New York City",
+    "billing_postal": "11205",
+    "currency": "usd",
+    "cvv": "V",
+    "click_speed": "0.3487",
+    "transaction_env": "T",
+    "tempo": "101.993",
+    "valence": "0.443876228"
+  }
 })
 headers = {
-  'Content-Type': 'application/json'
+  'Content-Type': 'application/json',
+  'cache-control': "no-cache"
 }
 
 response = requests.request("POST", url, headers=headers, data=payload)
 
+# Response
 print(response.text)
